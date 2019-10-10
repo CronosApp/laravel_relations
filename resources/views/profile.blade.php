2975f22bdf723b1952d3ed77808fb444d65cf6c3
Luis J. Rodriguez <developer0606@gmail.com>
2019-10-10 13:58:28 -0400

    [ADD] Others Changes to profile view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -40,6 +40,64 @@
 
                       <hr>
 
+                      <h3>Posts</h3>
+
+                      <div class="row">
+                          @foreach($posts as $post)
+                              <div class="col-6">
+                                  <div class="card mb-3">
+                                      <div class="row">
+                                          <div class="col-md-4">
+                                              <img src="{{ $post->image->url }}" alt="{{ $post->image->url }}" class="card-img">
+                                          </div>
+                                          <div class="col-md-8">
+                                              <h5 class="card-title">{{ $post->name }}</h5>
+                                              <h6 class="card-subtitle text-muted">
+                                                  {{ $post->category->name }} |
+                                                  {{ $post->comments_count }}
+                                                  {{ Str::plural('comentario', $post->comments_count) }}
+                                              </h6>
+                                              <p class="card-text small">
+                                                  @foreach($post->tags as $tag)
+                                                    <span class="badge badge-light"> #{{ $tag->name }} </span>
+                                                  @endforeach
+                                              </p>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          @endforeach
+                      </div>
+
+                      <hr>
+                      <h3>Videos</h3>
+
+                      <div class="row">
+                          @foreach($videos as $video)
+                              <div class="col-6">
+                                  <div class="card mb-3">
+                                      <div class="row">
+                                          <div class="col-md-4">
+                                              <img src="{{ $video->image->url }}" alt="{{ $video->image->url }}" class="card-img">
+                                          </div>
+                                          <div class="col-md-8">
+                                              <h5 class="card-title">{{ $video->name }}</h5>
+                                              <h6 class="card-subtitle text-muted">
+                                                  {{ $video->category->name }} |
+                                                  {{ $video->comments_count }}
+                                                  {{ Str::plural('comentario', $video->comments_count) }}
+                                              </h6>
+                                              <p class="card-text small">
+                                                  @foreach($video->tags as $tag)
+                                                      <span class="badge badge-light"> #{{ $tag->name }} </span>
+                                                  @endforeach
+                                              </p>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          @endforeach
+                      </div>
                   </div>
               </div>
           </div>
